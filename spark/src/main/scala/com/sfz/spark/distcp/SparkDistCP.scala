64e1b3fcdef78de5f72eefdaf1536f09c86aecf9
sillyMadman <251562072@qq.com>
2021-09-03 17:07:29 +0800

    1.spark模块提交SparkDistCP

--- spark/src/main/scala/com/sfz/spark/distcp/SparkDistCP.scala
+++ spark/src/main/scala/com/sfz/spark/distcp/SparkDistCP.scala
@@ -1,8 +1,11 @@
 package com.sfz.spark.distcp
 
+import org.apache.commons.cli.{DefaultParser, Options}
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.fs.{FileSystem, FileUtil, Path}
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * @author sfz
@@ -12,44 +15,70 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SparkDistCP {
   def main(args: Array[String]): Unit = {
 
+    val options = new Options()
+    options.addOption("i", "ignore failure ", false, "ignore failures")
+    options.addOption("m", "max concurrence ", true, "max concurrence")
+    val parser = new DefaultParser()
+    val cmd = parser.parse(options, args)
 
-    val source = "/tmp/test_lhw"
-    val target = "/tmp/test_lhw2"
-
-    val configuration = new Configuration()
-    configuration.set("fs.defaultFS", "hdfs://sdc01:8020")
-    val fileSystem = FileSystem.get(configuration)
-
-    //获取文件目录及文件夹
-    val statuses = fileSystem.listStatus(new Path(source))
-    for (fileStatus <- statuses) {
-      // 如果是文件
-      if (fileStatus.isFile()) {
-        System.out.println("f:" + fileStatus.getPath().getName());
-      } else {
-        System.out.println("d:" + fileStatus.getPath().getName());
-      }
+    if (args.length < 2) {
+      println("SparkDistCP -m 10 -i input output ")
+      System.exit(-1)
     }
-    val conf = new SparkConf();
+
+    val input = args(args.length - 2)
+    val output = args(args.length - 1)
+
+    val IGNORE_FAILURE = cmd.hasOption("i")
+    val MAX_CONNCURRENCE = if (cmd.hasOption("m")) cmd.getOptionValue("m").toInt
+    else 2
+
+
+    val conf = new SparkConf()
     conf.setAppName("SparkDistCP")
       .setMaster("local")
     val sc = new SparkContext(conf)
 
-    val fileRdd = sc.makeRDD(statuses)
+    val fileSystem = FileSystem.get(sc.hadoopConfiguration)
+
+//    val configuration = new Configuration()
+//    configuration.set("fs.defaultFS", "hdfs://sdc01:8020")
+//    val  fileSystem= FileSystem.get(configuration)
 
 
-    fileRdd.foreach(x => {
-      println(x.getPath)
-      if(x.isDirectory()){
-        println(x.getPath)
-        val configuration = new Configuration()
-        configuration.set("fs.defaultFS", "hdfs://sdc01:8020")
-        val fileSystem = FileSystem.get(configuration)
-        fileSystem.mkdirs(new Path("/tmp/test_lhw2/"+x.getPath.getName))
 
+    val fileList = fileSystem.listFiles(new Path(input), true)
+
+    val arrayBuffer = ArrayBuffer[String]()
+
+
+    while (fileList.hasNext) {
+      val path = fileList.next().getPath.toString
+      arrayBuffer.append(path)
+      println(path)
+    }
+
+    val rdd = sc.parallelize(arrayBuffer, MAX_CONNCURRENCE)
+    rdd.foreachPartition(it => {
+      val conf = new Configuration()
+      val fs = FileSystem.get(conf)
+
+      while (it.hasNext) {
+        val src = it.next()
+        val tgt = src.replace(input, output)
+        try {
+          FileUtil.copy(fs, new Path(src), fs, new Path(tgt), false, conf)
+        } catch {
+          case ex: Exception =>
+            if (IGNORE_FAILURE) println("ignore failure when copy")
+            else throw ex
+        }
       }
-//
+
+
     })
+
+
 
 
   }
