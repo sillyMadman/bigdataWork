778a64373d715adb02df8bc46653173bebfc752e
sillyMadman <251562072@qq.com>
2021-10-08 14:29:18 +0800

    1.提交presto-hyperloglog题目一和题目三

--- presto-hyperloglog-test/README.md
+++ presto-hyperloglog-test/README.md
@@ -11,6 +11,20 @@ HyperLogLog算法在Presto的应用
 
 
 
+## 题目一
+
+
+### HyperLogLog的用途或大数据场景下的实际案例
+
+用户大量数据情况下去重统计:
+
+统计注册 IP 数  
+统计每日访问 IP 数  
+统计页面实时 UV 数  
+统计在线用户数  
+统计每天搜索不同词条的个数  
+统计真实文章阅读数  
+
 ## 题目二
 
 ### 准备
@@ -50,3 +64,13 @@ group by begindate,media;
 ### 运行结果截图
 
 ![题目二完成截图.png](题目二完成截图.png)
+
+## 题目三
+
+### 准备
+
+[PrestoJdbcTest](src/main/java/com/sfz/presto/PrestoJdbcTest.java)
+
+### 运行结果截图
+
+![题目三完成截图.png](题目三完成截图.png)
--- presto-hyperloglog-test/src/main/java/com/sfz/presto/PrestoJdbcTest.java
+++ presto-hyperloglog-test/src/main/java/com/sfz/presto/PrestoJdbcTest.java
@@ -5,6 +5,7 @@ package com.sfz.presto;/**
  */
 
 import java.sql.*;
+import java.util.Properties;
 
 /**
  * @Description: jdbc to presto
@@ -13,26 +14,66 @@ import java.sql.*;
  */
 public class PrestoJdbcTest {
 
-    public static void main(String[] args) throws  SQLException {
+    public static void main(String[] args) {
+         String sql;
+        if (args.length != 0) {
+            sql= args[0];
+        } else {
+          sql = "select * from shafengzi_base";
+        }
+
 
         try {
             Class.forName("com.facebook.presto.jdbc.PrestoDriver");
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+
+            System.out.println("Failed to load presto jdbc driver.");
+            System.exit(-1);
         }
-        String url = "jdbc:presto://106.15.194.185:9090/hive/default?user=student03&SSL=true";
-        Connection connection = DriverManager.getConnection(url);
-        Statement stmt = connection.createStatement();
+        Connection connection = null;
+        Statement statement = null;
+        try {
+//            String   url = "jdbc:presto://172.16.63.32:9090/hive/default";
+            String url = "jdbc:presto://106.15.194.185:9090/hive/default";
+            Properties properties = new Properties();
+            properties.setProperty("user", "hadoop");
+            // 创建连接对象。
+            connection = DriverManager.getConnection(url, properties);
+            // 创建Statement对象。
+            statement = connection.createStatement();
+            // 执行查询。
+            ResultSet rs = statement.executeQuery(sql);
+            // 获取结果。
+            int columnNum = rs.getMetaData().getColumnCount();
+            int rowIndex = 0;
+            while (rs.next()) {
+                rowIndex++;
+                for (int i = 1; i <= columnNum; i++) {
+                    System.out.println("Row " + rowIndex + ", Column " + i + ": " + rs.getString(i));
+                }
+            }
+        } catch (SQLException e) {
 
-//        ResultSet rs = stmt.executeQuery("show tables");
-        ResultSet rs = stmt.executeQuery("select * from shafengzi_base");
+            System.out.println("Exception thrown."+e);
+        } finally {
+            // 销毁Statement对象。
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (Throwable t) {
+                    System.out.println(t);
+                }
+            }
+            // 关闭连接。
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Throwable t) {
+                    System.out.println(t);
+                }
 
-        while (rs.next()) {
-            System.out.println("rs.getString(1) = " + rs.getString(1));
 
+            }
         }
-
-        rs.close();
-
     }
 }
